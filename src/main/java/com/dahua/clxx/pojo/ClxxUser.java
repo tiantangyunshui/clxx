b11f6d23c08eda963985db662cf6609bd1d0ba2e
tiantangyunshui <1292531625@qq.com>
2021-03-13 13:47:55 +0800

    东八区时间

--- src/main/java/com/dahua/clxx/pojo/ClxxUser.java
+++ src/main/java/com/dahua/clxx/pojo/ClxxUser.java
@@ -3,14 +3,23 @@ package com.dahua.clxx.pojo;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel("用户信息")
 public class ClxxUser extends Model<ClxxUser> {
+    @ApiModelProperty("id")
     private long id;
+    @ApiModelProperty("学号")
     private String no;
+    @ApiModelProperty("姓名")
     private String userName;
+    @ApiModelProperty("密码")
     private String password;
+    @ApiModelProperty("电话")
     private String phone;
+    @ApiModelProperty("类型0老师，1学生")
     private String type;
 }
--- src/main/java/com/dahua/clxx/pojo/Person.java
+++ src/main/java/com/dahua/clxx/pojo/Person.java
@@ -1,12 +1,16 @@
 package com.dahua.clxx.pojo;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class Person {
+    @ApiModelProperty("id")
     private String id;
     private String no;
+    @ApiModelProperty("姓名")
     private String name;
+    @ApiModelProperty("电话")
     private String phone;
     private String type;
     private String faceImg;
